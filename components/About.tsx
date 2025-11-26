d5d5b287f0bf84b80b428f27c1df10b23005c29c
Rushikesh <hrishi210402@gmail.com>
2025-11-26 13:22:50 +0530

    mobile responsive

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
   ];
 
   return (
-<div className='roboto-condensed w-screen overflow-x-hidden md:h-[280vh] h-[220vh] py-20 px-10 flex justify-center bg-black z-1000 absolute'>
+<div className='roboto-condensed w-full box-border overflow-x-hidden md:h-[280vh] h-[180vh] py-20 px-10 flex justify-center bg-black z-1000 absolute'>
       <div className='text-white'>
         {/* Fixed unescaped quotes */}
         <h1 className='md:text-5xl text-lg text-center  italic'>
@@ -28,37 +28,35 @@ const About = () => {
 
         
 
-        <div className="relative md:top-40 top-50 md:left-20 left-70  w-[80vw] border-t border-gray-300"></div>
+        <div className="relative md:top-40 top-28 md:left-20 left-0  w-[90vw] border-t border-gray-300"></div>
 
-        <Link href="/Aboutme">
-              
-  <div className="relative md:top-12 md:left-200 top-15 left-100 md:w-[15vw] w-[60vw]  h-[30vh] rounded-full bg-white text-black flex justify-center items-center text-3xl overflow-hidden group">
-    <span className="lora relative z-10">About Me</span>
+        <Link href="/Aboutme">       
+  <div className="relative md:top-12 md:left-250 top-5 left-45 md:h-[30vh] md:w-[15vw] w-[40vw]  h-[20vh] rounded-full bg-white text-black flex justify-center items-center text-3xl overflow-hidden group">
+    <span className="lora relative md:text-4xl text-2xl z-10">About Me</span>
     <span className="absolute inset-0 bg-amber-100  scale-0 group-hover:scale-100  transition-transform duration-300 ease-out rounded-full "></span>
   </div>
 </Link>
 
 
-        <div className='md:text-xl relative md:top-40 top-0'>RECENT WORKS :</div>
+        <div className='md:text-xl md:left-23 relative md:top-40 top-20 left-10'>RECENT WORKS :</div>
 
-      <div className='md:w-0 w-full'>
-        <HoverExpand_001 images={images} className='relative md:top-50 top-30 md:w-[100vw] w-full  ' />
+      <div className='flex justify-center items-center h-[50vh] w-full mt-15'>
+        <HoverExpand_001 images={images} className='relative top-10 md:top-50  ' />
       </div>
-
+{/* className='relative md:top-50 top-30 md:w-[100vw] w-full  '  */}
         <Link href="/Work">
-          <InteractiveHoverButton className='md:top-80 top-80 left-100 md:left-130'>
+          <InteractiveHoverButton className='md:top-80 top-35 left-30 md:left-170'>
             Detailed Work
           </InteractiveHoverButton>
         </Link>
 
-        <AnimatedDiv>
-        <h1 className='roboto-condensed md:text-8xl text-5xl relative md:top-102 top-120 md:left-105 left-70'>
+        
+        <h1 className='roboto-condensed md:text-8xl text-5xl relative md:top-102 top-79 md:left-105 left-36'>
           &nbsp; &nbsp; Let&apos;s work <br /> together
         </h1>
-        </AnimatedDiv>
         <br />
 
-        <div className="bg-white border-white border-2 md:h-[15vh] h-[0vh] md:w-[8vw] rounded-full overflow-hidden relative md:top-45 md:left-93">
+        <div className="bg-white md:h-[15vh] h-[18vh] w-[35vw] md:w-[8vw] rounded-full overflow-hidden relative md:top-45 md:left-93 left-2 top-30">
           <Image
             className="h-full w-full object-cover"
             src="/photo1.jpg"
@@ -69,39 +67,39 @@ const About = () => {
           />
         </div>
 
-        <div className="relative md:top-140 top-120  md:left-30 left-70 w-[70vw] border-t border-gray-300"></div>
+        <div className="relative md:top-140 top-70 md:left-30 left-5 w-[70vw] border-t border-gray-300"></div>
 
         <Link href="/Contact">
-          <div className="relative md:top-110 top-100 md:left-200 left-120 md:w-[15vw] w-[40vw] h-[20vh] text-center  md:h-[30vh] rounded-full bg-white text-black flex justify-center items-center text-3xl overflow-hidden group">
+          <div className="relative md:top-110 top-50 md:left-200 left-50 md:w-[15vw] w-[40vw] h-[20vh] text-center  md:h-[30vh] rounded-full bg-white text-black flex justify-center items-center text-3xl overflow-hidden group">
           <span className="lora relative z-10">Contact Me</span>
           <span className="absolute inset-0 bg-amber-100 scale-0 group-hover:scale-100 transition-transform duration-300 ease-out rounded-full"></span>
           </div>
         </Link>
 
-        {/* <div className="relative md:top-45  md:left-250  text-white scale-130">
+        <div className="relative md:top-0 -top-10 left-95 md:left-250  text-white scale-130">
           <ArrowDownLeft/>
-        </div> */}
+        </div>
 
-        <div className="md:h-[5vh] h-[5vh] w-[45vw] md:w-[15vw] relative md:top-83 top-80 left-70 md:left-50 text-center p-2 border border-white rounded-full text-white bg-black 
+        <div className="md:h-[5vh] h-[5vh] w-[45vw] md:w-[15vw] relative md:top-83 top-45 left-5 md:left-50 text-center p-2 border border-white rounded-full text-white bg-black 
   transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-white hover:text-black">
   hrishi210402@gmail.com
 </div>
 
-<div className="md:h-[5vh] h-[5vh] w-[45vw] md:w-[10vw] relative md:top-74 top-85 md:left-120 left-70 text-center p-2 border border-white rounded-full text-white bg-black 
+<div className="md:h-[5vh] h-[5vh] w-[45vw] md:w-[10vw] relative md:top-74 top-50 md:left-120 left-5 text-center p-2 border border-white rounded-full text-white bg-black 
   transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-white hover:text-black">
   +91 7208553533
 </div>
 
 
 <div className="relative text-gray-300 md:absolute md:bottom-50  w-full">
-  <span className='relative md:left-0 md:top-0 top-120 left-80'>version</span>
-  <span className='relative left-120 md:left-220 md:top-0 top-120'>Socials</span>
+  <span className='relative md:left-10 md:top-10 top-70 left-70'>version</span>
+  <span className='relative left-2 md:left-220 md:top-9 top-70'>Socials</span>
 </div>
 
 
-        <div className='relative top-125 md:left-0 right-75 md:top-130 flex justify-between md:px-5 text-gray-300'>
-          <span className='flex relative md:bottom-0 bottom-0 md:left-0 left-150'> 2025 <Copyright/> Edition</span>
-          <span className="md:flex ">
+        <div className='relative top-75 md:left-0 right-75 md:top-130 flex justify-between md:px-5 text-gray-300'>
+          <span className='flex relative md:bottom-0 bottom-0 md:left-10 left-140'> 2025 <Copyright/> Edition</span>
+          <span className="md:flex relative left-13 md:-left-40">
             <Link004 href="https://www.instagram.com/emoji2104/">Instagram</Link004>
             <Link004 href="https://www.linkedin.com/in/contactrushikesh21/">LinkedIn</Link004>
             <Link004 href="https://github.com/rushikesh-2104">Github</Link004>
--- components/ui/skiper-ui/skiper62.tsx
+++ components/ui/skiper-ui/skiper62.tsx
@@ -1,129 +1,3 @@
-// "use client";
-
-// import { AnimatePresence, motion } from "framer-motion";
-// import { useEffect, useMemo, useState } from "react";
-// import { ArrowDownRight } from 'lucide-react';
-// import Image from "next/image";
-
-// import {
-//   ScrollVelocityContainer,
-//   ScrollVelocityRow,
-// } from "@/components/magicui/scroll-based-velocity";
-// import Nav from "@/components/Nav";
-
-// const Skiper62 = () => {
-//   const greetings = useMemo(
-//     () => ["नमस्कार", "नमस्ते", "નમસ્તે", "নমস্কার", "ਸਤ ਸ੍ਰੀ ਅਕਾਲ", "வணக்கம்", "Hello!"],
-//     []
-//   );
-
-//   const [key, setKey] = useState(0);
-//   const [showPortfolio, setShowPortfolio] = useState(false);
-
-//   useEffect(() => {
-//     if (key < greetings.length - 1) {
-//       const timer = setTimeout(() => setKey((prev) => prev + 1), 500);
-//       return () => clearTimeout(timer);
-//     } else {
-//       const timeout = setTimeout(() => setShowPortfolio(true), 1200);
-//       return () => clearTimeout(timeout);
-//     }
-//   }, [key, greetings.length]);
-
-//   return (
-//     <div className="h-full w-[100vw] z-100 bg-black text-white overflow-x-hidden">
-//       {/* === Landing Section === */}
-//       <AnimatePresence mode="wait">
-//         {!showPortfolio && (
-//           <motion.section
-//             key="landing"
-//             className="flex min-h-screen items-center justify-center"
-//             initial={{ opacity: 0 }}
-//             animate={{ opacity: 1 }}
-//             exit={{ opacity: 0, y: -200 }}
-//             transition={{ duration: 1, ease: "easeInOut" }}
-//           >
-//             <motion.h1
-//               key={key}
-//               initial={{ opacity: 0, y: 50 }}
-//               animate={{ opacity: 1, y: 0 }}
-//               exit={{ opacity: 0, y: -50 }}
-//               transition={{
-//                 duration: 0.8,
-//                 ease: "easeInOut",
-//               }}
-//               className="whitespace-nowrap text-6xl font-bold tracking-tight md:text-8xl"
-//             >
-//               {greetings[key]}
-//             </motion.h1>
-//           </motion.section>
-//         )}
-//       </AnimatePresence>
-
-//       {/* === Portfolio Section === */}
-//       {showPortfolio && (
-//         <div>
-//           <motion.section
-//             key="portfolio"
-//             initial={{ y: "100%", opacity: 0 }}
-//             animate={{ y: 0, opacity: 1 }}
-//             transition={{ duration: 1, ease: "easeInOut" }}
-//             className="flex h-screen flex-col items-center justify-center bg-white text-black"
-//           >
-//             <Nav/>
-
-//            <div className="hidden md:block bg-white h-[10vh] w-[20vw] absolute left-0 bottom-100 rounded-r-full z-1000">
-//   <div className="flex justify-around py-2">
-//     <span className="text-l py-5 px-5 scale-150 wrap-break-word">
-//       Located in India
-//     </span>
-//     <span>
-//       <Image 
-//         src="/world.gif" 
-//         alt="World" 
-//         width={80} 
-//         height={80} 
-//         className="h-[8vh] w-[5vw]" 
-//       />
-//     </span>
-//   </div>
-// </div>
-
-
-//             <div className="absolute z-1000 md:left-260 left-8 bottom-35 md:bottom-125 text-white scale-130">
-//               <ArrowDownRight/>
-//             </div>
-            
-//             <span className="absolute z-1000 md:left-270 md:bottom-90 bottom-10 text-white md:text-5xl left-10 text-4xl">
-//               Software Developer <br /> & Designer
-//             </span>
-            
-
-//             <div className="bg-black w-screen flex justify-center">
-//               <Image 
-//                 src="/rushi.png" 
-//                 alt="Rushikesh" 
-//                 width={900} 
-//                 height={1000}
-//                 quality={100} 
-//                 className="relative h-[100vh]  bottom-0 " 
-//               />
-//             </div>
-
-//             <ScrollVelocityContainer className="lora absolute text-white top-110 md:top-140 scale-160 md:scale-180 text-9xl ">
-//               <ScrollVelocityRow baseVelocity={10} direction={1}>
-//                 Rushikesh Mhatre -
-//               </ScrollVelocityRow>
-//             </ScrollVelocityContainer>
-//           </motion.section>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export { Skiper62 };
-
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
@@ -146,7 +20,6 @@ const Skiper62 = () => {
   const [key, setKey] = useState(0);
   const [showPortfolio, setShowPortfolio] = useState(false);
 
-  // ✅ Check if intro already played
   useEffect(() => {
     const hasSeenIntro = sessionStorage.getItem("hasSeenIntro");
     if (hasSeenIntro) {
@@ -154,7 +27,6 @@ const Skiper62 = () => {
     }
   }, []);
 
-  // ✅ Greeting animation logic
   useEffect(() => {
     if (showPortfolio) return;
 
@@ -172,8 +44,7 @@ const Skiper62 = () => {
   }, [key, greetings.length, showPortfolio]);
 
   return (
-    <div className="h-full w-[100vw] z-100 bg-black text-white overflow-x-hidden">
-      {/* === Landing Section (Intro) === */}
+    <div className="h-full w-full  z-100 bg-black text-white overflow-x-hidden">
       <AnimatePresence mode="wait">
         {!showPortfolio && (
           <motion.section
@@ -198,18 +69,17 @@ const Skiper62 = () => {
         )}
       </AnimatePresence>
 
-      {/* === Portfolio Section === */}
       {showPortfolio && (
         <motion.section
           key="portfolio"
           initial={{ y: "100%", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="flex h-screen flex-col items-center justify-center bg-white text-black"
+          className="flex h-screen  flex-col items-center justify-center bg-white text-black overflow-x-hidden"
         >
           <Nav />
 
-          <div className="hidden md:block bg-white h-[10vh] w-[20vw] absolute left-0 bottom-100 rounded-r-full z-1000">
+          <div className="hidden md:block bg-white h-[10vh] w-[20vw] absolute left-0 rounded-r-full z-1000">
             <div className="flex justify-around py-2">
               <span className="text-l py-5 px-5 scale-150 wrap-break-word">
                 Located in India
@@ -226,26 +96,27 @@ const Skiper62 = () => {
             </div>
           </div>
 
-          <div className="absolute z-1000 md:left-260 left-8 bottom-35 md:bottom-125 text-white scale-130">
+          <div className="absolute md:left-260 left-8 bottom-35 md:bottom-125 text-white scale-130">
             <ArrowDownRight />
           </div>
 
-          <span className="absolute z-1000 md:left-270 md:bottom-90 bottom-10 text-white md:text-5xl left-10 text-4xl">
+          <span className="absolute z-1 md:left-270 md:bottom-90 bottom-10 text-white md:text-5xl left-10 text-4xl">
             Software Developer <br /> & Designer
           </span>
 
-          <div className="bg-black w-screen flex justify-center">
+          {/* ✅ FIXED HERE */}
+          <div className="bg-black h-full w-full max-w-full flex justify-center overflow-x-hidden">
             <Image
               src="/rushi.png"
               alt="Rushikesh"
               width={900}
-              height={1000}
+              height={100}
               quality={100}
-              className="relative h-[100vh] bottom-0"
+              className="h-full bottom-0"
             />
           </div>
 
-          <ScrollVelocityContainer className="lora absolute text-white top-110 md:top-140 scale-160 md:scale-180 text-9xl">
+          <ScrollVelocityContainer className="lora absolute  w-full overflow-x-hidden text-white top-110 md:top-140 scale-100 md:scale-180 text-9xl">
             <ScrollVelocityRow baseVelocity={10} direction={1}>
               Rushikesh Mhatre -
             </ScrollVelocityRow>
@@ -257,4 +128,3 @@ const Skiper62 = () => {
 };
 
 export { Skiper62 };
-
